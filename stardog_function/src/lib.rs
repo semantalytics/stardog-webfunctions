9b9444a80705d4289c55fd651699249385787506
Zachary Whitley <zachary.whitley@gmail.com>
2022-05-16 14:33:50 -0400

    Remove cardinality estimate function

--- stardog_function/src/lib.rs
+++ stardog_function/src/lib.rs
@@ -26,20 +26,3 @@ extern {
 extern {
     pub fn mappingDictionaryGet(buf_addr: i64) -> *mut c_char;
 }
-
-#[no_mangle]
-pub extern fn cardinality_estimate(subject: *mut c_char) -> *mut c_char {
-    let subject = unsafe { CStr::from_ptr(subject).to_str().unwrap() };
-
-    let values: Value = serde_json::from_str(subject).unwrap();
-    let estimate = values["results"]["bindings"][0]["value_0"]["value"].as_str().unwrap();
-    let accuracy = values["results"]["bindings"][0]["value_1"]["value"].as_str().unwrap();
-
-    let result = json!({
-      "head": {"vars":["value_0", "value_1"]}, "results":{"bindings":[
-            {"value_0":{"type":"literal","value": estimate}, "value_1":{"type":"literal","value": accuracy}}
-        ]}
-    }).to_string().into_bytes();
-
-    unsafe { CString::from_vec_unchecked(result) }.into_raw()
-}
